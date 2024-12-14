f4d773fdbc341419366612897ab08cce9ec11fad
David Norrman <d.norrman@sportradar.com>
2024-12-14 23:55:49 +0100

    clean day 12 part 1 solution

--- 2024/12/day12.py
+++ 2024/12/day12.py
@@ -3,7 +3,6 @@ directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 def read_input(file_path):
-    garden_map = []
     plant_locations = {}
 
     with open(file_path, 'r') as file:
@@ -13,19 +12,17 @@ def read_input(file_path):
                 if plant not in plant_locations:
                     plant_locations[plant] = set()
                 plant_locations[plant].add((i, j))
-            garden_map.append(garden_line)
-    return garden_map, plant_locations
+    return plant_locations
 
 
-def get_plant_region(garden_map,
-                     plant, location,
+def get_plant_region(locations, current_location,
                      area, perimiter,
                      visited):
 
-    if location not in visited:
-        visited.add(location)
+    if current_location not in visited:
+        visited.add(current_location)
 
-    adjacent_plants = get_adjacent_plants(garden_map, location, plant)
+    adjacent_plants = get_adjacent_plants(locations, current_location)
     area += 1
     perimiter += (4 - len(adjacent_plants))
 
@@ -36,37 +33,34 @@ def get_plant_region(garden_map,
     for adjacent_plant in adjacent_plants:
         if adjacent_plant not in visited:
             area, perimiter, visited = \
-                get_plant_region(garden_map,
-                                 plant, adjacent_plant,
+                get_plant_region(locations,
+                                 adjacent_plant,
                                  area, perimiter,
                                  visited)
     return area, perimiter, visited
 
 
-def get_adjacent_plants(garden_map, location, plant):
+def get_adjacent_plants(locations, current_location):
     adjacent_plants = set()
     for direction in directions:
-        neighbor = (location[0] + direction[0], location[1] + direction[1])
-        if in_garden_bounds(garden_map, neighbor) \
-                and garden_map[neighbor[0]][neighbor[1]] == plant:
+        neighbor = (
+            current_location[0] + direction[0],
+            current_location[1] + direction[1]
+        )
+        if neighbor in locations:
             adjacent_plants.add(neighbor)
     return adjacent_plants
 
 
-def in_garden_bounds(garden_map, location):
-    return 0 <= location[0] < len(garden_map) \
-            and 0 <= location[1] < len(garden_map[0])
-
-
 if __name__ == "__main__":
-    garden_map, plant_locations = read_input('input.in')
+    plant_locations = read_input('input.in')
 
     total_cost = 0
     for plant, locations in plant_locations.items():
         while len(locations) > 0:
             start_location = next(iter(locations))
             area, perimiter, visited = get_plant_region(
-                garden_map, plant, start_location, 0, 0, set()
+                locations, start_location, 0, 0, set()
             )
             # print(f"Plant {plant} - {area} * {perimiter} = {area * perimiter}")
             total_cost += area * perimiter
