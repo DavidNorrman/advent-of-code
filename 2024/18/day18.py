3728e2eb12d647a288d175d26ef562534bda8821
DavidNorrman <24862299+DavidNorrman@users.noreply.github.com>
2025-01-10 21:27:19 +0100

    add solution day 18 p1 with djikstras

---
+++ 2024/18/day18.py
@@ -0,0 +1,102 @@
+import heapq
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+    
+    def __lt__(self, other):
+        return (self.x, self.y) < (other.x, other.y)
+
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+
+
+directions = [
+    Vector(0, 1), 
+    Vector(0, -1),
+    Vector(1, 0),
+    Vector(-1, 0)
+]
+
+
+def read_input(file):
+    coordinates = []
+    with open(file) as f:
+        for line in f:
+            split_line = line.strip().split(',')
+            coordinates.append((int(split_line[0]), int(split_line[1])))
+    return coordinates
+
+def get_grid(x_len, y_len):
+    grid = []
+    for _ in range(x_len):
+        grid.append(['.'] * y_len)
+    return grid
+
+def simulate_byte_fall(grid, coordinates, nr_of_bytes):
+    for i, coordinate in enumerate(coordinates):
+        if i == nr_of_bytes:
+            break
+        grid[coordinate[1]][coordinate[0]] = '#'
+
+def find_path(grid, start, end):
+    queue = [(0, start)]
+    distances = {start: 0}
+    previous_nodes = {start: None}
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if current_node == end:
+            path = []
+            while current_node:
+                path.append(current_node)
+                current_node = previous_nodes[current_node]
+            return path[::-1]
+
+        for neighbor in neighbors(grid, current_node):
+            distance = current_distance + 1
+            if neighbor not in distances or distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous_nodes[neighbor] = current_node
+                heapq.heappush(queue, (distance, neighbor))
+    return None
+
+def neighbors(grid, location):
+    for velocity in directions:
+        neighbor = location + velocity
+        if in_bounds(grid, neighbor) and grid[neighbor.y][neighbor.x] != '#':
+            yield neighbor
+
+def in_bounds(grid, location):
+        return 0 <= location.x < len(grid[0]) and 0 <= location.y < len(grid)
+
+
+if __name__ == '__main__':
+    coordinates = read_input('input.in')
+    width, height = 71, 71
+    grid = get_grid(width, height)
+
+    simulate_byte_fall(grid, coordinates, 1024)
+    start = Vector(0, 0)
+    end = Vector(width - 1, height - 1)
+
+    escape_path = find_path(grid, start, end)
+
+    for location in escape_path:
+        grid[location.y][location.x] = 'O'
+    
+    for row in grid:
+        print(' '.join(row))
+
+    print("Escape path length: ", len(escape_path) - 1)
