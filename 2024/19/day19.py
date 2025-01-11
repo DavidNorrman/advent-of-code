3a6cbd2cc03a89380f1c61600865cad06a3f0ab2
DavidNorrman <24862299+DavidNorrman@users.noreply.github.com>
2025-01-11 17:16:45 +0100

    add day 19 part 2

--- 2024/19/day19.py
+++ 2024/19/day19.py
@@ -6,7 +6,6 @@ def read_input(file):
         towels = [line.strip() for line in f]
     return colors, towels
 
-
 def color_scheme_is_possible(colors, towel, memo = {}):
     if towel in memo:
         return memo[towel]
@@ -21,9 +20,21 @@ def color_scheme_is_possible(colors, towel, memo = {}):
     memo[towel] = False
     return False
 
+def color_combinations(colors, towel, memo = {}):
+    if not towel:
+        return 1
+    if towel in memo:
+        return memo[towel]
+    count = sum(color_combinations(colors, towel[len(color):], memo) for color in get_fitting_colors(colors, towel))
+    memo[towel] = count
+    return count
+
 def get_fitting_colors(colors, towel_colors):
     return (color for color in colors if towel_colors.startswith(color))
 
 if __name__ == '__main__':
     colors, towels = read_input('input.in')
+    # Part 1
     print(len([towel for towel in towels if color_scheme_is_possible(colors, towel)]))
+    # Part 2
+    print(sum(color_combinations(colors, towel) for towel in towels))
