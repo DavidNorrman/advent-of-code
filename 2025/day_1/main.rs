65d99701674b6b2a53b87c97c255ed7ebdf5ff34
DavidNorrman <davidthenorrman@gmail.com>
2025-12-01 11:52:17 +0100

    a little cleanup

--- 2025/day_1/main.rs
+++ 2025/day_1/main.rs
@@ -3,10 +3,47 @@
 fn main() {
     let lines = include_str!("input.in").lines().collect::<Vec<&str>>();
 
+    let _part_one_zeros = _part_one(&lines);
+    println!("Number of zeros in part one: {}", _part_one_zeros);
+
+    let _part_two_zeros = _part_two(&lines);
+    println!("Number of zeros in part two: {}", _part_two_zeros);
+}
+
+fn _part_one(rotations: &Vec<&str>) -> i32 {
     let mut zeros = 0;
     let mut current_pos = 50;
 
-    for line in lines {
+    for line in rotations {
+        let direction = &line[0..1];
+        let amount = &line[1..].parse::<i32>().unwrap();
+
+        match direction {
+            "L" => {
+                current_pos = (current_pos - amount).rem_euclid(100);
+                if current_pos == 0 {
+                    zeros += 1;
+                }
+            }
+            "R" => {
+                current_pos = (current_pos + amount) % 100;
+                if current_pos == 0 {
+                    zeros += 1;
+                }
+            }
+            _ => {}
+        }
+
+    }
+
+    zeros
+}
+
+fn _part_two(rotations: &Vec<&str>) -> i32 {
+    let mut zeros = 0;
+    let mut current_pos = 50;
+
+    for line in rotations {
         let direction = &line[0..1];
         let amount = &line[1..].parse::<i32>().unwrap();
 
@@ -23,36 +60,6 @@ fn main() {
             }
             _ => {}
         }
-    }
-
-    println!("Number of zeros: {}", zeros);
-}
-
-
-fn _part_one_password(rotations: &Vec<&str>) -> i32 {
-    let mut zeros = 0;
-    let mut current_pos = 50;
-
-    for line in rotations {
-        let direction = &line[0..1];
-        let amount = &line[1..].parse::<i32>().unwrap();
-
-        match direction {
-            "L" => {
-                current_pos = (current_pos - amount + 100) % 100;
-                if current_pos == 0 {
-                    zeros += 1;
-                }
-            }
-            "R" => {
-                current_pos = (current_pos + amount) % 100;
-                if current_pos == 0 {
-                    zeros += 1;
-                }
-            }
-            _ => {}
-        }
-
     }
 
     zeros
